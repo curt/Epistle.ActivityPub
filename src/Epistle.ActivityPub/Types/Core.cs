38cc8f51d037b66d707290aafc36767eb1468517
Curt Gilman <14070+curt@users.noreply.github.com>
2024-04-21 07:30:00 -0700

    make type virtual, nuget properties, bump version

--- src/Epistle.ActivityPub/Types/Core.cs
+++ src/Epistle.ActivityPub/Types/Core.cs
@@ -5,7 +5,7 @@ public partial class Core : ICore
 {
     private string? _type;
 
-    public string? Type { get { return _type ?? GetType().Name; } set { _type = value; } }
+    public virtual string? Type { get { return _type ?? GetType().Name; } set { _type = value; } }
 
     [JsonPropertyName("@context")]
     public IEnumerableTriple? JsonLdContext { get; set; }
